0c24a8a8e25660d6df66dbf5c365e1a2ebd26971
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-05 15:59:35 +0100

    Fix comments

--- program.js
+++ program.js
@@ -1,22 +1,20 @@
-// process.stdin.setEncoding('utf-8');
-// process.stdin.on('readable', function () {
-// 	const input = process.stdin.read();
-// 	if (input !== null) {
-// 		const instruction = input.toString().trim();
-// 		switch(instruction) {
-// 		 	case '/exit':
-// 				process.stdout.write('Quitting app!\n');
-// 				process.exit();
-// 		 		break;
-// 		 	default:
-// 				process.stderr.write('Wrong instruction\n');
-// 		 		break;
-// 		}
-// 	}
-// })
-console.log(	
-`
-	Language: ${process.env.LANGUAGE}
-	Node version: ${process.versions.node}
-`
-)
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function () {
+	const input = process.stdin.read();
+	if (input !== null) {
+		const instruction = input.toString().trim();
+		switch(instruction) {
+		 	case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+		 		break;
+		 	case '/info' :
+				process.stdout.write(`\nLanguage: ${process.env.LANGUAGE}\nNode version: ${process.versions.node}\n`
+			 	);
+			 	break;
+		 	default:
+				process.stderr.write('Wrong instruction\n');
+		 		break;
+		}
+	}
+});
