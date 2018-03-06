33b18f7e5447f55c84268f4855b1dc42bc578433
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-06 16:15:38 +0100

    Prettify script

--- program.js
+++ program.js
@@ -1,20 +1,19 @@
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function () {
+process.stdin.on('readable', function() {
 	const input = process.stdin.read();
 	if (input !== null) {
 		const instruction = input.toString().trim();
-		switch(instruction) {
-		 	case '/exit':
+		switch (instruction) {
+			case '/exit':
 				process.stdout.write('Quitting app!\n');
 				process.exit();
-		 		break;
-		 	case '/info' :
-				process.stdout.write(`\nLanguage: ${process.env.LANGUAGE}\nNode version: ${process.versions.node}\n`
-			 	);
-			 	break;
-		 	default:
+				break;
+			case '/info':
+				process.stdout.write(`\nLanguage: ${process.env.LANGUAGE}\nNode version: ${process.versions.node}\n`);
+				break;
+			default:
 				process.stderr.write('Wrong instruction\n');
-		 		break;
+				break;
 		}
 	}
 });
